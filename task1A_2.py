e2bea9d6ab2b01b3b373331cbdb35bc557e89d8a
wesjoen <jn298501@dal.ca>
2019-02-15 09:58:18 -0800

    task1A.2

---
+++ task1A_2.py
@@ -0,0 +1,12 @@
+
+def count_the_article(list):
+  number = 0
+  for i in list:
+    number = number +1
+  print(number)
+
+
+
+
+list = ['a','the','at','run','to','and','or','for','an','this']
+count_the_article(list)
